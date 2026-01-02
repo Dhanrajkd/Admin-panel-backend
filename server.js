0d82634c83d11e81216480e4f90b7dd694787cf8
Dhanrajkd <dhanrajd158@gmail.com>
2026-01-03 00:08:50 +0530

    updated backend

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
-import multer from 'multer'
 import studentroutes from "./Routes/Studentrouter.js"
 import Employeerouter from "./Routes/Employeerouter.js"
 import Admins from './Routes/Adminrouter.js'
@@ -19,9 +18,9 @@ app.use(cors({
 app.listen(PORT,()=>{
     console.log(`server running in${PORT}`)
 })
-app.get('/',(req,res)=>{
-    res.send("server is running")
-})
+app.get("/", (req, res) => {
+  res.send("Admin Panel Backend Live 🚀");
+});
 mongoose.connect("mongodb+srv://dhanrajd158_db_user:test1234@cluster0.dmc0nz2.mongodb.net/")
 .then(()=>{
     console.log("Db connected")
@@ -29,11 +28,11 @@ mongoose.connect("mongodb+srv://dhanrajd158_db_user:test1234@cluster0.dmc0nz2.mo
 .catch((err)=>{
     console.log(err)
 })
-app.use("/uploads", express.static("uploads"));
+app.use("/api/admin", Admins);
+app.use("/api/admin/uploads", express.static("uploads"));
 app.use("/employee_img",express.static("employee_img"))
-app.use('/',studentroutes)
-app.use("/",Employeerouter)
-app.use("/",Admins)
-app.use("/",Course)
-app.use("/",Batch)
-app.use("/",Fees)
+app.use('/api/admin',studentroutes)
+app.use("/api/admin",Employeerouter)
+app.use("/api/admin",Course)
+app.use("/api/admin",Batch)
+app.use("/api/admin",Fees)
